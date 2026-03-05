b51103a4b35304a6b231ec6cf9eb4c4c296b99e8
jimmy <jimishukurow@gmail.com>
2026-03-05 14:26:55 +0500

    removed RabbitMQ config to common package

--- src/main/java/info/jemsit/product_service/config/RabbitMQConfig.java
+++ src/main/java/info/jemsit/product_service/config/RabbitMQConfig.java
@@ -1,44 +1,19 @@
 package info.jemsit.product_service.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static info.jemsit.common.data.constants.RabbitMQConstants.*;
-
 
 @Configuration
 public class RabbitMQConfig {
-    @Bean
-    public Queue eventQueue() {
-        return new Queue(MEDIA_QUEUE, true);
-    }
 
     @Bean
-    public TopicExchange mediaExchange() {
-        return new TopicExchange(MEDIA_EXCHANGE);
-    }
-
-    @Bean
-    public Binding mediaBinding(Queue eventQueue, TopicExchange mediaExchange) {
-        return BindingBuilder.bind(eventQueue).to(mediaExchange).with(MEDIA_KEY);
-    }
-
-
-    @Bean
-    public Jackson2JsonMessageConverter messageConverter(){
-        return new Jackson2JsonMessageConverter();
-    }
-    @Bean
-    public RabbitTemplate rabbitTemplate (ConnectionFactory connectionFactory){
+    public RabbitTemplate rabbitTemplate (ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
+        rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
     }
 }
